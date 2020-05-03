7e0d42609a7156a1170ce89684140d282d4812e3
Rajat Shenoy <rajatshenoy@gmail.com>
2020-05-03 17:17:24 +0530

    added formbuilder formgroup validators for login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private fromBuilder : FormBuilder) { }
+  Login : FormGroup
 
   ngOnInit() {
+    this.Login = this.fromBuilder.group(
+      {
+       adminEmail:['', Validators.required],
+       Password:['',Validators.required], 
+      }
+    );
   }
-
+  onSubmit(){
+    console.log(this.Login.value)
+  }
 }
