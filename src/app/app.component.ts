ff003206ab460d3000b4d5f1af8aa5c35bf3c3be
Rajat Shenoy <rajatshenoy@gmail.com>
2020-05-03 20:49:07 +0530

    routes problem13

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,6 @@ import { Component, ViewEncapsulation } from '@angular/core';
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  encapsulation: ViewEncapsulation.None
 })
 export class AppComponent {
   title = 'portfolio';
